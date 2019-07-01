1e699c636ae4cbfe82ae7aabf799cb3f219c9c70
Simon Zhang <muyezhang@gmail.com>
2019-07-01 22:44:00 +0800

    Initial commit

---
+++ plugins/svg-icon.js
@@ -0,0 +1,9 @@
+// plugins/global-components.js
+import Vue from 'vue'
+import svgIcon from '~/components/SvgIcon'
+
+Vue.component('svg-icon', svgIcon)
+
+const requireAll = requireContext => requireContext.keys().map(requireContext)
+const req = require.context('~/assets/icons/svg', false, /\.svg$/)
+requireAll(req)
